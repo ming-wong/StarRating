7aee728c0922c84aea73d75f61e3bd5e5ee52441
Ming Wong <js923030238@hotmail.com>
2016-02-23 15:26:57 +0800

    使用jstarbox實作星星評分

---
+++ StarRating/Controllers/StarRatingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StarRating.Controllers
+{
+    public class StarRatingController : Controller
+    {
+        // GET: StarRating
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
