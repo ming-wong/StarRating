daea1b20254dbf633ca67ad4ee9bb84b3e93ae31
Ming Wong <js923030238@hotmail.com>
2016-02-23 10:28:25 +0800

    第一次上傳，同步處裡

---
+++ StarRating/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StarRating.Startup))]
+namespace StarRating
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
